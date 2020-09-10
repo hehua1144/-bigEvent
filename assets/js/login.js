0a35b895d56fe324506c2a8287d81ae4130d74b4
hehuahua <543348063@qq.com>
2020-09-10 15:33:35 +0800

    完成了登录及注册页面的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,6 +9,7 @@ $(function () {
         $('.reg_box').hide()
     })
     var form = layui.form
+    var layer = layui.layer
     form.verify({
         pwd: [
             /^[\S]{6,12}$/
@@ -20,6 +21,34 @@ $(function () {
                 return '两次密码输入不一致'
             }
         }
+    })
+    $('#reg_form').on('submit', function (e) {
+        e.preventDefault()
+        $.post('/api/reguser', {
+            username: $('#reg_form [name="uesrname"]').val(), password: $('#reg_form [name="password"]').val()
+        }, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录')
+            $('#reg_box').click()
+        })
+    })
+    $('#login_form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功！')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
     })
 
 })
