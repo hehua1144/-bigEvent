80e9fef7d7460d3ccd40027d0ea5482db8a9d2e6
hehuahua <543348063@qq.com>
2020-09-10 10:37:29 +0800

    登录注册静态页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,9 +4,22 @@ $(function () {
         $('.login_box').hide()
         $('.reg_box').show()
     })
-    $('#regId').on('click', function () {
+    $('#reg_box').on('click', function () {
         $('.login_box').show()
         $('.reg_box').hide()
+    })
+    var form = layui.form
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $('#pwd_Id').val()
+            if (pwd !== value) {
+                return '两次密码输入不一致'
+            }
+        }
     })
 
 })
